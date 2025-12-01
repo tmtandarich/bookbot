c787e7c7261dad256edfb1def381aca33b8049ab
tmtandarich <tmtandarich@gmail.com>
2025-11-30 21:36:50 -0500

    add report via print_report function

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from stats import get_num_words, get_char_dict
+from stats import (
+    get_num_words,
+    get_char_dict,
+    chars_dict_to_sorted_list,
+)
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_dict(text)
-    print(f"Found {num_words} total words")
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)   
+    print_report(book_path, num_words, chars_sorted_list) 
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    
+    print("============= END ===============")
 
 
 main()
