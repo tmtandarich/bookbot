81287c3b720c3d47e0995683d25c4f5671b30cac
tmtandarich <tmtandarich@gmail.com>
2025-12-01 21:50:30 -0500

    completed bookbot with report and error

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import sys
+
 from stats import (
     get_num_words,
     get_char_dict,
     chars_dict_to_sorted_list,
 )
 
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_dict(text)
